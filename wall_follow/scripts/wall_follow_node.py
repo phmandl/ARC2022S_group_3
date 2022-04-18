0ee9e261f3b7592d2de745292085c7280447167a
Philipp Mandl <ph.mandl@gmail.com>
2022-04-18 15:19:09 +0200

    modifed weighting of car; velocity speed of car adapted to middle ray

--- wall_follow/scripts/wall_follow_node.py
+++ wall_follow/scripts/wall_follow_node.py
@@ -3,10 +3,12 @@ from __future__ import print_function
 from ast import If
 import sys
 import math
+from urllib.parse import MAX_CACHE_SIZE
 import numpy as np
 
 #ROS Imports
 import rospy
+from std_msgs.msg import Float64
 from sensor_msgs.msg import Image, LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
 from dynamic_reconfigure.server import Server
@@ -15,18 +17,18 @@ from wall_follow.cfg import dyn_reconfigConfig
 #WALL FOLLOW PARAMS
 ANGLE_RANGE = 270 # Hokuyo 10LX has 270 degrees scan
 NR_SCANS = 1080
-THETA_ANGLE = 60
+THETA_ANGLE = 48
 ANGLE_INCREMENT = 0.004365153145045042
 INDEX_RIGHT_SCAN_RAY = 180
 INDEX_LEFT_SCAN_RAY = 900
-LOOKAHEAD_DISTANCE = 0.5
+LOOKAHEAD_DISTANCE = 1.4
 DESIRED_DISTANCE_RIGHT = 1.0 # meters
-DESIRED_DISTANCE_LEFT = 1.0
-MAX_VELOCITY = 1.50 # meters per second
+DESIRED_DISTANCE_LEFT = 1.2
 CAR_LENGTH = 0.50 # Traxxas Rally is 20 inches or 0.5 meters
 HZ = 100
 TS = 1.0/HZ
 NR_RAYS_A_TO_B = int(np.deg2rad(THETA_ANGLE)/ANGLE_INCREMENT)
+MAX_VELOCITY = 4.75
 
 class WallFollow:
     """ Implement Wall Following on the car
@@ -35,10 +37,12 @@ class WallFollow:
         #Topics & Subs, Pubs
         lidarscan_topic = '/scan'
         drive_topic = '/nav'
+        Dtp1_topic = '/dtp1'
 
         # self.odom_sub = rospy.Subscriber("/odom",Odometry,self.odom_callback)      
         self.lidar_sub = rospy.Subscriber(lidarscan_topic, LaserScan, self.lidar_callback)
         self.drive_pub = rospy.Publisher(drive_topic, AckermannDriveStamped, queue_size=100)
+        self.Dtp1_pub = rospy.Publisher(Dtp1_topic, Float64, queue_size=100)
 
         # dynamic reconfigure server
         self.srv = Server(dyn_reconfigConfig, self.dynCallback)
@@ -47,9 +51,9 @@ class WallFollow:
         self.r = rospy.Rate(HZ)
 
         # PID Stuff
-        self.Kp = 1
-        self.Ki = 0.1
-        self.Kd = 0.0
+        self.Kp = 0.8
+        self.Ki = 0.0
+        self.Kd = 0.001
         self.N = 100
         self.e0 = 0
         self.e1 = 0
@@ -61,7 +65,8 @@ class WallFollow:
         self.Dtp1 = 0
         self.alpha = 0
         self.angle = 0
-        self.maxVelo = MAX_VELOCITY
+        self.velocity = 0
+        self.midRayRange = 0
 
     def pid_control(self):
 
@@ -78,6 +83,7 @@ class WallFollow:
 
         # invert angle argument
         self.angle = -1*self.u0
+        # self.angle = np.sign(self.angle)*np.min([np.abs(self.angle),np.deg2rad(30)])
 
         # update values
         self.e1 = self.e0
@@ -87,23 +93,24 @@ class WallFollow:
 
     def followLeft(self):
 
-        # CHECK VELOCITY ON LAST STEERING ANGLE
-        abs_deg_angle = np.abs(np.rad2deg(self.angle))
-        if (abs_deg_angle <= 10):
-            # velocity = 1.5
-            velocity = self.maxVelo
-        elif (abs_deg_angle <= 20):
-            velocity = 1.0
-        else:
-            velocity = 0.5
+        # Execute PID based on current scan
+        self.pid_control()
+
+        # calculate velocity
+        self.calcVelocity()
 
         # Ackermann Msgs
         drive_msg = AckermannDriveStamped()
         drive_msg.header.stamp = rospy.Time.now()
         drive_msg.header.frame_id = "laser"
         drive_msg.drive.steering_angle = self.angle
-        drive_msg.drive.speed = self.maxVelo
+        drive_msg.drive.speed = self.velocity
         self.drive_pub.publish(drive_msg)
+
+        # Dtp1 publisher
+        dtp1_msg = Float64()
+        dtp1_msg.data = self.Dtp1
+        self.Dtp1_pub.publish(dtp1_msg)
 
     def lidar_callback(self, data):
         """ 
@@ -127,20 +134,44 @@ class WallFollow:
         # calculate error
         self.e0 = DESIRED_DISTANCE_LEFT - self.Dtp1
 
+        # save data
+        nr_rays = len(data.ranges)
+        mid_ray_int = int(nr_rays/2)
+        self.midRayRange = data.ranges[mid_ray_int]
+
     def dynCallback(self, config, level):
         self.Kp = config.Kp
         self.Ki = config.Ki
         self.Kd = config.Kd
         self.N = config.N
-        self.maxVelo = config.maxVelo
         return config
+
+    def calcVelocity(self):
+        # CHECK VELOCITY ON LAST STEERING ANGLE
+        abs_deg_angle = np.abs(np.rad2deg(self.angle))
+        if (abs_deg_angle <= 10):
+            # Check if distance in front of vehicle is greater than a threshold    
+            if self.midRayRange > 10:
+                self.velocity = 7.0
+            else:
+                self.velocity = MAX_VELOCITY
+        else: 
+            inter_velocity = MAX_VELOCITY*np.abs(np.cos(self.angle))
+            self.velocity = np.max([1.0,inter_velocity])
+            # self.velocity = np.max([1.0,np.abs(8.0*np.cos(3*np.deg2rad(abs_deg_angle)))])
+
+        # if (abs_deg_angle <= 10):
+        #     # Check if distance in front of vehicle is greater than a threshold    
+        #     self.velocity = 3.0
+        # else: 
+        #     self.velocity = 1.5
+            
 
 def main(args):
     rospy.init_node("wall_follow", anonymous=True)
     wf = WallFollow()
 
     while not rospy.is_shutdown():
-        wf.pid_control()
         wf.followLeft()
         wf.r.sleep()
 
